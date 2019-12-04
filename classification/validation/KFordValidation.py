12ebdded92fb66046a9e760754cde3a5ef64ec8a
151250176 <151250176@smail.nju.edu.cn>
2019-12-04 11:38:25 +0800

    add time and f2 score

--- classification/validation/KFordValidation.py
+++ classification/validation/KFordValidation.py
@@ -1,15 +1,17 @@
+from time import time
 from sklearn.model_selection import KFold
 import numpy as np
 
 
 def k_ford_validation(x, y, model):
+    cur_time = time()
     if not hasattr(model, "fit"):
         raise AttributeError("model doesn't have fit method")
     if not hasattr(model, "predict"):
         raise AttributeError("model doesn't have predict method")
 
     kf = KFold(n_splits=5, random_state=0)
-    accuracy, precision_res, recall_res, f1_res = 0.0, 0.0, 0.0, 0.0
+    accuracy, precision_res, recall_res, f1_res, f2_res = 0.0, 0.0, 0.0, 0.0, 0.0
 
     for train_index, test_index in kf.split(x):
         train_x, train_y = x[train_index], y[train_index]
@@ -23,12 +25,15 @@ def k_ford_validation(x, y, model):
         precision_res += precision(score, test_y)
         recall_res += recall(score, test_y)
         f1_res += f1(score, test_y)
+        f2_res += f2(score, test_y)
 
     print("----------------")
+    print("Time: ", time() - cur_time)
     print("accuracy: ", accuracy / 5)
     print("precision: ", precision_res / 5)
     print("recall: ", recall_res / 5)
     print("f1: ", f1_res / 5)
+    print("f2: ", f2_res / 5)
     print("----------------")
 
 
@@ -66,3 +71,13 @@ def f1(out, y):
         return 0
 
     return 2 * precision_val * recall_val / (precision_val + recall_val)
+
+
+def f2(out, y):
+    precision_val = precision(out, y)
+    recall_val = recall(out, y)
+
+    if precision_val + recall_val == 0:
+        return 0
+
+    return 5 * precision_val * recall_val / (4 * precision_val + recall_val)
