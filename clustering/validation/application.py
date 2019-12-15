251b404515e6c75e9ae640ac041e90d8d7f426d0
JackieTien97 <JackieTien@foxmail.com>
2019-12-15 21:34:04 +0800

    application code

--- clustering/validation/application.py
+++ clustering/validation/application.py
@@ -25,24 +25,24 @@ sse = estimator.inertia_
 silhouette = silhouette_score(data, label)
 calinski_harabasz = calinski_harabasz_score(data, label)
 print("lib result: \t", " k: ", k, "\tsse: ", sse, "\tsilhouette: ", silhouette, "\tcalinski_harabaz_score: ", calinski_harabasz)
-#
-# for i in range(k):
-#     cluster_k = raw_data[label == i]
-#     a = {}
-#     for j in range(len(cluster_k)):
-#         key = cluster_k[j][1]
-#         if key in a:
-#             a[key] += 1
-#         else:
-#             a[key] = 1
-#     for class_num in range(1, 14):
-#         if class_num in a:
-#             a[class_num] = a[class_num] / len(raw_data[raw_data[:, 1] == class_num])
-#
-#     items = sorted(a.items(), key=lambda x: x[1], reverse=True)
-#
-#     rank = np.mean(cluster_k)
-#     print(i, rank, items)
+
+for i in range(k):
+    cluster_k = raw_data[label == i]
+    a = {}
+    for j in range(len(cluster_k)):
+        key = cluster_k[j][1]
+        if key in a:
+            a[key] += 1
+        else:
+            a[key] = 1
+    for class_num in range(1, 14):
+        if class_num in a:
+            a[class_num] = a[class_num] / len(raw_data[raw_data[:, 1] == class_num])
+
+    items = sorted(a.items(), key=lambda x: x[1], reverse=True)
+
+    rank = np.mean(cluster_k)
+    print(i, rank, items)
 
 
 
